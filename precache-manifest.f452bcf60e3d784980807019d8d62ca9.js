467daeecab3bf9ddc174355e16ea277ae12e3118
Alexei Zababurin <852gin@gmail.com>
2019-08-06 15:39:32 +0300

    Updates

---
+++ precache-manifest.f452bcf60e3d784980807019d8d62ca9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc2521c083cdd4371db7b9627e2d1800",
+    "url": "/textengine/index.html"
+  },
+  {
+    "revision": "fa3732f95d5fa03b4101",
+    "url": "/textengine/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/textengine/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "fa3732f95d5fa03b4101",
+    "url": "/textengine/static/js/main.dd7ca22b.chunk.js"
+  },
+  {
+    "revision": "a43c7345ef17ffb6d993",
+    "url": "/textengine/static/js/runtime~main.c48e5de5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/textengine/static/media/logo.5d5d9eef.svg"
+  }
+]);
